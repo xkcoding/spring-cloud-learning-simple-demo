ba47762e1d95c51504a4e9c9960bcc5165702979
Yangkai.Shen <237497819@qq.com>
2018-08-14 17:49:19 +0800

    :sparkles: 支持配置文件更新配置中心下发刷新配置

--- config-client/src/main/java/com/xkcoding/configclient/controller/IndexController.java
+++ config-client/src/main/java/com/xkcoding/configclient/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.xkcoding.configclient.controller;
 
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import java.util.Map;
  * @modified: yangkai.shen
  */
 @RestController
+@RefreshScope
 @RequestMapping("/api")
 public class IndexController {
 	@Value("${demo.name}")
